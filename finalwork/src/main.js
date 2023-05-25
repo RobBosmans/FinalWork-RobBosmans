4fe1fc3b8770f5707e2a942c58234f5870865119
RobBosmans <56031137+RobBosmans@users.noreply.github.com>
2023-05-25 16:26:26 +0200

    Removed chart because it wasnt needed

--- finalwork/src/main.js
+++ finalwork/src/main.js
@@ -1,9 +1,5 @@
-import Chart from 'chart.js/auto';
-
 // Array to store heart rate data for the graph
 let hrData = new Array(200).fill(10);
-
-let chart; 
 
 // Variable to store the heart rate value element
 let heartRateValueElement;
@@ -27,7 +23,7 @@ async function connect(props) {
 }
 
 // Function to update the heart rate value and graph
-function updateHeartRate(event, chart) {
+function updateHeartRate(event) {
   const heartRate = event.target.value.getInt8(1);
   const prev = hrData[hrData.length - 1];
   hrData[hrData.length] = heartRate;
@@ -35,58 +31,11 @@ function updateHeartRate(event, chart) {
   let arrow = '';
   if (heartRate !== prev) arrow = heartRate > prev ? '⬆' : '⬇';
   heartRateValueElement.textContent = heartRate;
-  chart.data.datasets[0].data = hrData;
-  chart.update();
 }
 
 // Event listener for the connect button
 document.getElementById('connectButton').addEventListener('click', () => {
   heartRateValueElement = document.getElementById('heartRateValue');
-  connect({ onChange: (event) => updateHeartRate(event, chart) }).then((char) => {
-    // Create canvas element
-    const canvas = document.createElement('canvas');
-    canvas.width = 1000;
-    canvas.height = 400;
-    document.body.appendChild(canvas);
-    
-    // Create line chart
-    const ctx = canvas.getContext('2d');
-    chart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: new Array(200).fill(0).map((_, i) => i),
-        datasets: [{
-          data: hrData,
-          borderColor: '#F15025',
-          borderWidth: 2,
-          pointRadius: 0,
-          pointHitRadius: 0,
-          backgroundColor: '#F15025',
-          fill: true
-        }]
-      },
-      options: {
-        responsive: true,
-        animation: false,
-        legend: {
-          display: false
-        },
-        scales: {
-          x: {
-            display: false,
-            grid: {
-              display: false,
-            },
-          },
-          y: {
-            display: false,
-            grid: {
-              display: false,
-            },
-          }
-        }
-      }
-    });
-  });
+  connect({ onChange: (event) => updateHeartRate(event) })
 });
 
